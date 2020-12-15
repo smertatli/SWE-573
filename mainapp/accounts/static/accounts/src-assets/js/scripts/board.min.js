d97db6eee8679832b50fedee0ccd2095a5abb2d5
smertatli <smertatli@gmail.com>
2020-12-15 18:18:39 +0300

    login/registration fixed
    login/registration fixed

--- mainapp/accounts/static/accounts/src-assets/js/scripts/board.min.js
+++
@@ -1,1 +0,0 @@
-dragula([document.getElementById('left-defaults'), document.getElementById('right-defaults'),document.getElementById('left-defaults-l'), document.getElementById('right-defaults-l')]);
