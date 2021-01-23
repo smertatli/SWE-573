d2facbbd91633ffbacb82bab6af5782dedc8caf6
smertatli <smertatli@gmail.com>
2021-01-23 13:58:01 +0300

    production fixes

--- mainapp/trendspotter/settings.py
+++ mainapp/trendspotter/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_URL = "/staticfiles/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_URL = '/login'
