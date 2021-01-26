6a434bbe99db21d3e73af5b0f26704970b1a474e
smertatli <smertatli@gmail.com>
2021-01-26 17:14:07 +0300

    check for delaying task to avoid deadlock in db

--- mainapp/tracker/tweet_collector.py
+++ mainapp/tracker/tweet_collector.py
@@ -81,156 +81,161 @@ def insertTweets(main, query_name=''):
     df_annotation_entity.to_sql('df_annotation_entity', engine, if_exists='append', index=False)
     df_entities.to_sql('df_entities', engine, if_exists='append', index=False)
 
-    conn = engine.connect() 
+    now = datetime.datetime.now()
+    if now.minute % 3 == 0:
+        print('PERFORMING JOIN: ', minute)
+        conn = engine.connect() 
 
-    try:
-        conn.execute('select * from tweet_main_table')
-        conn.execute("""
-            with base as (
-                select tweet_tweet_id, b.username, tweet_text, tweet_author_id, tweet_created_at, 
-                tweet_conversation_id, tweet_in_reply_to_user_id, 
-                tweet_retweet_count, tweet_reply_count, tweet_like_count, tweet_quote_count,
-                a.key, a.query_name
-                from df_merge a, df_users b
-                where a.query_name ='{0}' and a.key ='{1}'
-                and a.query_name = b.query_name and a.key = b.key and a.tweet_author_id = b.id
-            ),
-            base2 as (
+        try:
+            conn.execute('select * from tweet_main_table')
+            conn.execute("""
                 with base as (
-                    select distinct 
-                        tweet_id, 
-                        lower(domain_name) as domain_name, lower(entity_name) as entity_name
-                    from df_annotations a, df_annotation_domain b, df_annotation_entity c
-                    where a.query_name ='{0}' 
-                    and a.key ='{1}'
-                    and a.query_name = b.query_name and a.domain_id = b.domain_id
-                    and a.query_name = c.query_name and a.entity_id = c.entity_id
+                    select tweet_tweet_id, b.username, tweet_text, tweet_author_id, tweet_created_at, 
+                    tweet_conversation_id, tweet_in_reply_to_user_id, 
+                    tweet_retweet_count, tweet_reply_count, tweet_like_count, tweet_quote_count,
+                    a.key, a.query_name
+                    from df_merge a, df_users b
+                    where a.query_name ='{0}' and a.key ='{1}'
+                    and a.query_name = b.query_name and a.key = b.key and a.tweet_author_id = b.id
+                ),
+                base2 as (
+                    with base as (
+                        select distinct 
+                            tweet_id, 
+                            lower(domain_name) as domain_name, lower(entity_name) as entity_name
+                        from df_annotations a, df_annotation_domain b, df_annotation_entity c
+                        where a.query_name ='{0}' 
+                        and a.key ='{1}'
+                        and a.query_name = b.query_name and a.domain_id = b.domain_id
+                        and a.query_name = c.query_name and a.entity_id = c.entity_id
 
-                )
-                select tweet_id, string_agg(concat(domain_name,'=',entity_name), ' || ') domain_entities
+                    )
+                    select tweet_id, string_agg(concat(domain_name,'=',entity_name), ' || ') domain_entities
+                    from base
+                    group by tweet_id
+                ),
+                base3 as (
+                    select tweet_id, string_agg(case when category = 'mentions' then username end,'||') mentions,
+                    string_agg(case when category = 'hashtags' then tag end,'||') hashtags,
+                    string_agg(case when category = 'annotations' and type_of ='Person' then normalized_text end,'||') annotation_persons,
+                    string_agg(case when category = 'annotations' and type_of ='Organization' then normalized_text end,'||') annotation_org, 
+                    string_agg(case when category = 'annotations' and type_of ='Product' then normalized_text end,'||') annotation_product,
+                    string_agg(case when category = 'annotations' and type_of ='Place' then normalized_text end,'||') annotation_place,
+                    string_agg(case when category = 'annotations' and type_of ='Other' then normalized_text end,'||') annotation_other
+                    from df_entities
+                    where query_name ='{0}' and key ='{1}'
+                    group by tweet_id
+                ),
+                base4 as (
+                    select 
+                        a.tweet_id, type, username, reference_id, text,
+                        string_agg(distinct case when type ='retweeted' then lower(username) end, ',') retweeted_user,
+                        string_agg(distinct case when type ='quoted' then lower(username) end, ',') quoted_user,
+                        string_agg(distinct case when type ='replied_to' then lower(username) end, ',') replied_to_user,
+                        string_agg(distinct case when type ='retweeted' then a.reference_id  end, '||') retweeted_tweet_id,
+                        b.text as retweeted_text
+                    from 
+                        df_tweets_referenced a, df_tweets_referenced_meta b, df_users c
+                    where 
+                        a.query_name ='{0}' and a.key ='{1}' and
+                        a.query_name = b.query_name and b.query_name = c.query_name and
+                        a.key = b.key and b.key = c.key and
+                        a.reference_id = b.tweet_id and b.author_id = c.id 
+                    group by a.tweet_id, type, username, reference_id, text
+                ),
+                base5 as (
+                select base.*, base2.domain_entities, 
+                mentions, hashtags, annotation_org, annotation_product, annotation_place, annotation_other, annotation_persons,
+                retweeted_user, quoted_user, replied_to_user, retweeted_text, retweeted_tweet_id,
+                type, base4.username as ref_username, text
                 from base
-                group by tweet_id
-            ),
-            base3 as (
-                select tweet_id, string_agg(case when category = 'mentions' then username end,'||') mentions,
-                string_agg(case when category = 'hashtags' then tag end,'||') hashtags,
-                string_agg(case when category = 'annotations' and type_of ='Person' then normalized_text end,'||') annotation_persons,
-                string_agg(case when category = 'annotations' and type_of ='Organization' then normalized_text end,'||') annotation_org, 
-                string_agg(case when category = 'annotations' and type_of ='Product' then normalized_text end,'||') annotation_product,
-                string_agg(case when category = 'annotations' and type_of ='Place' then normalized_text end,'||') annotation_place,
-                string_agg(case when category = 'annotations' and type_of ='Other' then normalized_text end,'||') annotation_other
-                from df_entities
-                where query_name ='{0}' and key ='{1}'
-                group by tweet_id
-            ),
-            base4 as (
-                select 
-                    a.tweet_id, type, username, reference_id, text,
-                    string_agg(distinct case when type ='retweeted' then lower(username) end, ',') retweeted_user,
-                    string_agg(distinct case when type ='quoted' then lower(username) end, ',') quoted_user,
-                    string_agg(distinct case when type ='replied_to' then lower(username) end, ',') replied_to_user,
-                    string_agg(distinct case when type ='retweeted' then a.reference_id  end, '||') retweeted_tweet_id,
-                    b.text as retweeted_text
-                from 
-                    df_tweets_referenced a, df_tweets_referenced_meta b, df_users c
-                where 
-                    a.query_name ='{0}' and a.key ='{1}' and
-                    a.query_name = b.query_name and b.query_name = c.query_name and
-                    a.key = b.key and b.key = c.key and
-                    a.reference_id = b.tweet_id and b.author_id = c.id 
-                group by a.tweet_id, type, username, reference_id, text
-            ),
-            base5 as (
-            select base.*, base2.domain_entities, 
-            mentions, hashtags, annotation_org, annotation_product, annotation_place, annotation_other, annotation_persons,
-            retweeted_user, quoted_user, replied_to_user, retweeted_text, retweeted_tweet_id,
-            type, base4.username as ref_username, text
-            from base
-            left join base2 on base.tweet_tweet_id = base2.tweet_id
-            left join base3 on base.tweet_tweet_id = base3.tweet_id
-            left join base4 on base.tweet_tweet_id = base4.tweet_id
-            )
-            INSERT INTO tweet_main_table select * from base5
-        """.format(query_name, time_key))
-    except:
-        conn.execute("""
-            with base as (
-                select tweet_tweet_id, b.username, tweet_text, tweet_author_id, tweet_created_at, 
-                tweet_conversation_id, tweet_in_reply_to_user_id, 
-                tweet_retweet_count, tweet_reply_count, tweet_like_count, tweet_quote_count,
-                a.key, a.query_name
-                from df_merge a, df_users b
-                where a.query_name ='{0}' and a.key ='{1}'
-                and a.query_name = b.query_name and a.key = b.key and a.tweet_author_id = b.id
-            ),
-            base2 as (
+                left join base2 on base.tweet_tweet_id = base2.tweet_id
+                left join base3 on base.tweet_tweet_id = base3.tweet_id
+                left join base4 on base.tweet_tweet_id = base4.tweet_id
+                )
+                INSERT INTO tweet_main_table select * from base5
+            """.format(query_name, time_key))
+        except:
+            conn.execute("""
                 with base as (
-                    select distinct 
-                        tweet_id, 
-                        lower(domain_name) as domain_name, lower(entity_name) as entity_name
-                    from df_annotations a, df_annotation_domain b, df_annotation_entity c
-                    where a.query_name ='{0}' 
-                    and a.key ='{1}'
-                    and a.query_name = b.query_name and a.domain_id = b.domain_id
-                    and a.query_name = c.query_name and a.entity_id = c.entity_id
+                    select tweet_tweet_id, b.username, tweet_text, tweet_author_id, tweet_created_at, 
+                    tweet_conversation_id, tweet_in_reply_to_user_id, 
+                    tweet_retweet_count, tweet_reply_count, tweet_like_count, tweet_quote_count,
+                    a.key, a.query_name
+                    from df_merge a, df_users b
+                    where a.query_name ='{0}' and a.key ='{1}'
+                    and a.query_name = b.query_name and a.key = b.key and a.tweet_author_id = b.id
+                ),
+                base2 as (
+                    with base as (
+                        select distinct 
+                            tweet_id, 
+                            lower(domain_name) as domain_name, lower(entity_name) as entity_name
+                        from df_annotations a, df_annotation_domain b, df_annotation_entity c
+                        where a.query_name ='{0}' 
+                        and a.key ='{1}'
+                        and a.query_name = b.query_name and a.domain_id = b.domain_id
+                        and a.query_name = c.query_name and a.entity_id = c.entity_id
 
-                )
-                select tweet_id, string_agg(concat(domain_name,'=',entity_name), ' || ') domain_entities
+                    )
+                    select tweet_id, string_agg(concat(domain_name,'=',entity_name), ' || ') domain_entities
+                    from base
+                    group by tweet_id
+                ),
+                base3 as (
+                    select tweet_id, string_agg(case when category = 'mentions' then username end,'||') mentions,
+                    string_agg(case when category = 'hashtags' then tag end,'||') hashtags,
+                    string_agg(case when category = 'annotations' and type_of ='Person' then normalized_text end,'||') annotation_persons,
+                    string_agg(case when category = 'annotations' and type_of ='Organization' then normalized_text end,'||') annotation_org, 
+                    string_agg(case when category = 'annotations' and type_of ='Product' then normalized_text end,'||') annotation_product,
+                    string_agg(case when category = 'annotations' and type_of ='Place' then normalized_text end,'||') annotation_place,
+                    string_agg(case when category = 'annotations' and type_of ='Other' then normalized_text end,'||') annotation_other
+                    from df_entities
+                    where query_name ='{0}' and key ='{1}'
+                    group by tweet_id
+                ),
+                base4 as (
+                    select 
+                        a.tweet_id, type, username, reference_id, text,
+                        string_agg(distinct case when type ='retweeted' then lower(username) end, ',') retweeted_user,
+                        string_agg(distinct case when type ='quoted' then lower(username) end, ',') quoted_user,
+                        string_agg(distinct case when type ='replied_to' then lower(username) end, ',') replied_to_user,
+                        string_agg(distinct case when type ='retweeted' then a.reference_id  end, '||') retweeted_tweet_id,
+                        b.text as retweeted_text
+                    from 
+                        df_tweets_referenced a, df_tweets_referenced_meta b, df_users c
+                    where 
+                        a.query_name ='{0}' and a.key ='{1}' and
+                        a.query_name = b.query_name and b.query_name = c.query_name and
+                        a.key = b.key and b.key = c.key and
+                        a.reference_id = b.tweet_id and b.author_id = c.id 
+                    group by a.tweet_id, type, username, reference_id, text
+                ),
+                base5 as (
+                select base.*, base2.domain_entities, 
+                mentions, hashtags, annotation_org, annotation_product, annotation_place, annotation_other, annotation_persons,
+                retweeted_user, quoted_user, replied_to_user, retweeted_text, retweeted_tweet_id,
+                type, base4.username as ref_username, text
                 from base
-                group by tweet_id
-            ),
-            base3 as (
-                select tweet_id, string_agg(case when category = 'mentions' then username end,'||') mentions,
-                string_agg(case when category = 'hashtags' then tag end,'||') hashtags,
-                string_agg(case when category = 'annotations' and type_of ='Person' then normalized_text end,'||') annotation_persons,
-                string_agg(case when category = 'annotations' and type_of ='Organization' then normalized_text end,'||') annotation_org, 
-                string_agg(case when category = 'annotations' and type_of ='Product' then normalized_text end,'||') annotation_product,
-                string_agg(case when category = 'annotations' and type_of ='Place' then normalized_text end,'||') annotation_place,
-                string_agg(case when category = 'annotations' and type_of ='Other' then normalized_text end,'||') annotation_other
-                from df_entities
-                where query_name ='{0}' and key ='{1}'
-                group by tweet_id
-            ),
-            base4 as (
-                select 
-                     a.tweet_id, type, username, reference_id, text,
-                    string_agg(distinct case when type ='retweeted' then lower(username) end, ',') retweeted_user,
-                    string_agg(distinct case when type ='quoted' then lower(username) end, ',') quoted_user,
-                    string_agg(distinct case when type ='replied_to' then lower(username) end, ',') replied_to_user,
-                    string_agg(distinct case when type ='retweeted' then a.reference_id  end, '||') retweeted_tweet_id,
-                    b.text as retweeted_text
-                from 
-                    df_tweets_referenced a, df_tweets_referenced_meta b, df_users c
-                where 
-                    a.query_name ='{0}' and a.key ='{1}' and
-                    a.query_name = b.query_name and b.query_name = c.query_name and
-                    a.key = b.key and b.key = c.key and
-                    a.reference_id = b.tweet_id and b.author_id = c.id 
-                group by a.tweet_id, type, username, reference_id, text
-            ),
-            base5 as (
-            select base.*, base2.domain_entities, 
-            mentions, hashtags, annotation_org, annotation_product, annotation_place, annotation_other, annotation_persons,
-            retweeted_user, quoted_user, replied_to_user, retweeted_text, retweeted_tweet_id,
-            type, base4.username as ref_username, text
-            from base
-            left join base2 on base.tweet_tweet_id = base2.tweet_id
-            left join base3 on base.tweet_tweet_id = base3.tweet_id
-            left join base4 on base.tweet_tweet_id = base4.tweet_id
-            )
-            SELECT * INTO tweet_main_table from base5
-        """.format(query_name, time_key))
-    
-    conn.execute("""truncate
-                    df_merge ,
-                    df_users ,
-                    df_tweets_referenced ,
-                    df_tweets_referenced_meta ,
-                    df_annotations ,
-                    df_entities,
-                    df_media
-                """)
+                left join base2 on base.tweet_tweet_id = base2.tweet_id
+                left join base3 on base.tweet_tweet_id = base3.tweet_id
+                left join base4 on base.tweet_tweet_id = base4.tweet_id
+                )
+                SELECT * INTO tweet_main_table from base5
+            """.format(query_name, time_key))
+        
+        conn.execute("""truncate
+                        df_merge ,
+                        df_users ,
+                        df_tweets_referenced ,
+                        df_tweets_referenced_meta ,
+                        df_annotations ,
+                        df_entities,
+                        df_media
+                    """)
 
-    conn.close()
+        conn.close()
+    else:
+        print('SKIPPING JOIN: ', minute)
 
     engine.dispose()
